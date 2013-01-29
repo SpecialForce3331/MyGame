d315c01d28dd447dbe6aa92d3087345769f3a766
Sizov Sergei <Wolf-sf@mail.ru>
2013-01-29 13:58:28 +0600

    Начал прикручивать регистрацию и авторизацию пользователей
    -- main.js шлет аякс запросы на регистрацию и авторизацию
    -- engine.php серверный код по добавлению в базу учеток, и авторизации
    по ним
    -- main.php будет отображать текущие открытые игры, и даст возможность
    создать свою

---
+++ engine.php
@@ -0,0 +1,68 @@
+<?php
+	
+	//Данные для подключения к БД
+	$server = "localhost";
+	$username = "MyGame";
+	$password = "MyGame";
+	$database = "MyGame";
+	
+	$mysqli = new mysqli( $server, $username, $password, $database ); //Устанавливаем соединение в базой мускула
+	
+	if ( $mysqli->connect_errno )
+	{
+		echo "Failed to connect to MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error; //Не удалось установить соединение с базой мускула.
+	}
+	else
+	{
+		$mysqli->set_charset("utf8"); //Устанавливаем принудительно кодировку в UTF-8!
+	}
+	
+	
+
+	if ( $_GET["action"] == "auth" )
+	{
+		$_GET['login'] = strip_tags($_GET['login']);
+		$_GET['password'] = strip_tags($_GET['password']); //Убираем теги из логина и пароля ( XSS ).
+		
+		$result = $mysqli->query("SELECT `login` WHERE `login` = '".$_GET['login']."' AND `password` = '".md5($_GET['password'])."'" );
+		if ($mysqli->connect_errno)
+		{
+			echo "Failed to connect to MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
+				
+		}else{
+			$rows = $result->num_rows;
+			$userData = $result->fetch_array(MYSQLI_ASSOC);
+			$answer = array( "result" => $rows , "userData" => $userData);
+			echo json_encode( $answer ); //Отдаем данные в формате json
+			if( $rows == 1 )
+			{
+				session_start(); //Устанавливаем HTTP сессию и пишем логин в сессионную переменную
+				$_SESSION['login'] = $_GET['login'];
+			}
+		}
+		$result->close;
+	}
+	elseif( $_GET["action"] == "reg" )
+	{
+		//Убираем из полученных данных теги (XSS)
+		$_GET['login'] = strip_tags($_GET['login']);
+		$_GET['password'] = strip_tags($_GET['password']);
+		
+		$mysqli->query( "INSERT INTO `users` ( `login`, `password` ) VALUES ('".$_GET['login']."','".md5($_GET['password'])."')" );
+		if ($mysqli->error)
+		{
+			echo "Failed to insert data: (" . $mysqli->errno . ") " . $mysqli->error;
+				
+		}
+		else
+		{
+				
+			session_start(); //Устанавливаем HTTP сессию и пишем логин в сессионную переменную
+			$_SESSION['login'] = $_GET['login'];
+			$result = array("result" => $_SESSION['login']);
+			echo json_encode($result);
+		}
+		
+	}
+
+?>
--- main.js
+++ main.js
@@ -1,185 +1,28 @@
-//прописываем необходимые переменные
-var context;
-var drawingCanvas;
+//Как только страница прогружена, отображаем поля для авторизации.
+window.onload = function()
+{
+	var inputWindow = " Login:<br/><input type='text' id='login' /><br/>" +
+			"Password:<br/><input type='password' id='password' /><br/>" +
+			"<button onclick='auth();'>Авторизиация</button><br/>" +
+			"<button onclick='reg();'>Регистрация</button><br/>"
+	$("#mainWidow").empty()
+	$("#mainWindow").append(inputWindow);
 
-//Ширина и высота холста
-var canvasHeight = 260;
-var canvasWidth = 450;
-
-
-window.onload = function() {
-	
-	//устанавливаем нужные размеры холста
-	
-	//слушаем нажатие клавиш
-	window.addEventListener('keydown',doKeyDown,true); 
-	window.addEventListener('keyup',doKeyUp,true); 
-
-	//Записываем объект канваса в переменную
-	drawingCanvas = document.getElementById('game');
-	
-	//устанавливаем нужные размеры холста
-	drawingCanvas.setAttribute("height",canvasHeight);
-	drawingCanvas.setAttribute("width",canvasWidth);
-	
-	//почва для рисования
-	context = drawingCanvas.getContext('2d');
-	
-     // Рисуем землю
-     context.moveTo(0.5,canvasHeight-10);
-     context.lineTo(450,canvasHeight-10);
-     context.strokeStyle = "#000";
-     context.stroke();
-     
-     //рисуем игрока 
-     player = new user(10, canvasHeight-20, 10, 10);
-     player.id = 1;
-     player.color = "#000000";
-     player.draw();
-     
-     //рисуем второго игрока
-     player2 = new user(40, canvasHeight-20, 10, 10);
-     player2.id = 2;
-     player2.color = "#FF0000";
-     player2.draw();
-     
-   //рисуем третьего игрока
-     player3 = new user(80, canvasHeight-20, 10, 10);
-     player3.id = 3;
-     player3.color = "blue";
-     player3.draw();
-     
-   //отрисовываем игроков с частотой 60 fps, 24 кадра в секунду
-     	setInterval(function(){player.draw();}, 2.5); 
-    	setInterval(function(){player2.draw();}, 2.5);
-    	setInterval(function(){player3.draw();}, 2.5); 
-    	
-    	//гравитация
-    	setInterval(function(){gravity();}, 30 ); 
-    	
 }
-
-function user(x, y, width, height) //прототип игрока
+function auth()
 {
-	this.id;
-	this.myContext = context;
-	this.x = x;
-	this.y = y;
-	this.width = width;
-	this.height = height;
-	this.color;
-	this.mass = 2;
-	this.draw = function()
-	{
-		this.myContext.fillStyle = this.color;
-		this.myContext.fillRect(this.x, this.y, this.width, this.height);
-	}
-}
-
-
-function movePlayer(z) //передвижение игрока
-{
-	context.clearRect(player.x, player.y, 10, 10);
-	
-	if( (player.x + z) < canvasWidth && (player.x + z) > 0 ) //проверка на выход за границы
-	{
-		player.x = player.x + z;
-	}
-	
-	doSend(player.id + ',' + player.x +','+ player.y); //отправляем координаты и id через функцию файла wsclient.js
-}
-
-function movePlayer2(x,y) //движение второго игрока
-{
-	context.clearRect(player2.x, player2.y, 10, 10);
-	player2.x = x;
-	player2.y = y;
-}
-
-function movePlayer3(x,y) //движение второго игрока
-{
-	context.clearRect(player3.x, player3.y, 10, 10);
-	player3.x = x;
-	player3.y = y;
-}	
-
-function jumpPlayer() //прыжок
-{
-	context.clearRect(player.x, player.y, 10, 10);
-	player.y -= 80;
-	
-	doSend(player.id + ',' + player.x +','+ player.y); 
-}
-
-function gravity() //якобы гравитация ))
-{
-	if (player.y < (canvasHeight-20) ) //пока находится в воздухе
-		{
-			player.myContext.clearRect(player.x, player.y, 10, 10);
-			player.y += 10;
-			
-			doSend(player.id + ',' + player.x +','+ player.y);
+	$.ajax({								
+		url : 'engine.php',
+		async : false,
+		data : {
+			'action' : 'auth',
+			'login' : login,
+			'password' : password
+		},
+		dataType : "json",
+		success : function(data) {
+			alert(data.result);
+			document.location="/main.php";
 		}
-	else if(player.y > (canvasHeight-20) ) //если ниже уровня земли
-		{
-			player.myContext.clearRect(player.x, player.y, 10, 10);
-			player.y = 280;
-			
-			doSend(player.id + ',' + player.x +','+ player.y);
-		}
-}
-
-var forwardId; //id для интервалов ходьбы вперед и назад (чтобы не суммировалась скорость)
-var backId;
-
-function doKeyDown(event) //при нажатии клавиш управления
-{
-	if(event.keyCode == 68) //вперед
-		{
-			if( forwardId == null )
-				{
-					forwardId = setInterval(function(){movePlayer(5);}, 30 ); //идем пока клавиша нажата
-				}
-		}
-	else if( event.keyCode == 65) //назад
-		{
-			if( backId == null)
-				{
-					backId = setInterval(function(){movePlayer(-5);}, 30 ); //идем пока клавиша нажата
-				}
-		}
-	else if( event.keyCode == 87) //прыжок
-		{
-			jumpPlayer();
-		}
-	else if( event.keyCode == 83) //присед
-		{
-			downPlayer();
-		}
-	else if( event.keyCode == 68 && event.keyCode == 87 ) //прыжок со смещением вперед
-		{
-			jumpPlayer();
-			movePlayer(5, -1);
-		}
-	else if( event.keyCode == 65 && event.keyCode == 87 ) //прыжок со смещением назад
-		{
-			jumpPlayer();
-			movePlayer(-5, -1);
-		}
-}
-
-function doKeyUp( event ) //при отжатии клавиши вперед или назад
-{
-
-	if( event.keyCode == 68 ) //вперед
-	{
-		clearInterval( forwardId );
-		forwardId = null;
-	}
-	else if( event.keyCode == 65 ) //назад
-	{
-		clearInterval( backId );
-		backId = null;
-	}
-
+	})
 }
