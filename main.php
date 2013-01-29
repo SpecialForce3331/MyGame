c05ad7a25634258cba7ddd9d757b1c170757e1c5
Sizov Sergei <Wolf-sf@mail.ru>
2013-01-29 17:05:47 +0600

    bug fix

--- main.php
+++ main.php
@@ -1,14 +1,25 @@
 <?php
+session_start();
+
+if(!is_null($_SESSION["login"]))
+{
 	echo 
 		'<html>
 		<head>
 		<script type="text/javascript" src="jquery-1.8.3.js"></script>
+		<script type="text/javascript" src="main.js"></script>
+		<script type="text/javascript" src="game/wsclient.js"></script>
+			
 		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
 		<title>Welcome to MyGame</title>
 		</head>
 		<body>
-			<div align="center" style="padding-top:10%;" id="mainWindow"></div>
-			Test passed
+			<div align="center" style="padding-top:10%;" id="output"></div>
 		</body>
 		</html>';
+}
+else
+{
+	echo "<script>document.location='index.html'</script>";	
+}
 ?>
