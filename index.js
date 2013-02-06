2f07c24a9a5afbd3f4a259a68ef6f124fe01d3f3
Sizov Sergei <Wolf-sf@mail.ru>
2013-02-06 14:39:16 +0600

    bug fix

--- index.js
+++ index.js
@@ -1,34 +1,35 @@
 function auth()
 {
 	$.ajax({								
-		url : 'engine.php',
+		url : 'http://427044.dyn.ufanet.ru:8080/GameServer/mysql',
 		async : false,
 		data : {
 			'action' : 'auth',
 			'login' : document.getElementById("login").value,
 			'password' : document.getElementById("password").value
 		},
-		dataType : "json",
+		dataType : "jsonp",
 		success : function(data) {
-			if(data.result == 1)
+			if( data.result == 1 )
 			{
-				document.location="main.php";
+				alert("sucess");
 			}
-		}
+		},
+		error : function(data){alert("error");}
 	})
 }
 
 function reg()
 {
 	$.ajax({								
-		url : 'engine.php',
+		url : 'http://427044.dyn.ufanet.ru:8080/GameServer/mysql',
 		async : false,
 		data : {
 			'action' : 'reg',
 			'login' : document.getElementById("login").value,
 			'password' : document.getElementById("password").value
 		},
-		dataType : "json",
+		dataType : "jsonp",
 		success : function(data) {
 			alert(data.result);
 			document.location="main.php";
