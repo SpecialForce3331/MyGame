32ee4985fb1dc1e65c8f9f7f75b2fa26e7fa5f43
Sizov Sergei <Wolf-sf@mail.ru>
2013-02-22 21:59:03 +0600

    Revert "Revert "переломный момент""
    This reverts commit 420f65cfb758c11178b842d32ac165e5e740ad68.

--- main.js
+++ main.js
@@ -4,11 +4,10 @@ var output;
 window.onload = function()
 {
 	output = document.getElementById("output");
-	getLogin();
-	getCharacters();
+	checkSession();
 }
 
-function getLogin()
+function checkSession()
 {
 	$.ajax({								
 		url : 'http://427044.dyn.ufanet.ru:8080/GameServer/mysql',
@@ -18,11 +17,16 @@ function getLogin()
 		},
 		dataType : "jsonp",
 		success : function(data) {
-			if(data.result != "false")
+			if(typeof(data.result) != undefined)
 			{
 				document.getElementById("login").value = data.result;
+				getCharacters();
 			}
-		}
+		},
+		error: function(data)
+			{
+				document.location="/MyGame/index.html";
+			}
 	})
 }
 
@@ -32,17 +36,33 @@ function getCharacters()
 		url : 'http://427044.dyn.ufanet.ru:8080/GameServer/mysql',
 		async : false,
 		data : {
-			'action' : 'getCharacters',
-			'login' : document.getElementById("login").value
+			'action' : 'getCharacters'
 		},
 		dataType : "jsonp",
 		success : function(data) {
 			if(data.result != "false")
 			{
-				$("#characters").append("<div>" + data.result + "</div>");
+				for( var i = 0; i < data.result.length; i++ ) //перебираем массив с персонажами
+				{	
+					var result = data.result[i].split(","); //разбиваем значения персонажа по запятым
+					$("#characters").append("<div style='display:inline-block; border:solid 1px black; width: 200px;' id="+ i +"><button onclick='choise("+ result[1] +")'>Выбрать</button></div> "); //выводим контейнеры для персонажей в одну строку				
+					
+					for ( var x = 0; x < result.length; x++ ) //перебираем значения персонажа
+					{
+						$("#"+ i ).append("<div id='"+ i +"'>" + result[x] + "</div>");//выводим значения персонажей в столбец
+					}
+				}				
 			}
 		}
 	})
+}
+
+//установка роли при выборе персонажа
+var role;
+function choise(id)
+{
+	role = id;
+	alert("Персонаж выбран");
 }
 
 function writeToScreen(message) 
@@ -54,9 +74,64 @@ function writeToScreen(message)
 	output.appendChild(pre); 
 }
 
+function showCreateGame()
+{
+	$("#createGame").empty();
+	$("#createGame").append("" +
+			"Описание игры: <input type='text' id='title' /><br/>" +
+			"Если вы оставите поле Пароль пустым, то к вам сможет подключится кто угодно.<br/>" +
+			"Пароль к игре: <input type='text' id='password' /><br/>" +
+			"<button onclick='newGame()'>Создать Игру</button>");
+}
+
 function newGame()
 {
+	$.ajax({								
+		url : 'http://427044.dyn.ufanet.ru:8080/GameServer/mysql',
+		async : false,
+		data : {
+			'action' : 'newGame',
+			'title' : document.getElementById("title").value,
+			'password' : document.getElementById("password").value,
+			'role' : role
+		},
+		dataType : "jsonp",
+		success : function(data) {
+			if(data.result != "false")
+			{
+				document.getElementById("login").value = data.result;
+			}
+		}
+	})
 	
+	$("#createGame").empty();
+}
+
+function showGames()
+{
+	$.ajax({								
+		url : 'http://427044.dyn.ufanet.ru:8080/GameServer/mysql',
+		async : false,
+		data : {
+			'action' : 'showGames'
+		},
+		dataType : "jsonp",
+		success : function(data) {
+			if(data.result != "false")
+			{
+				for( var i = 0; i < data.result.length; i++ ) //перебираем массив с персонажами
+				{	
+					var result = data.result[i].split(","); //разбиваем значения игры по запятым
+					$("#games").append("<div style='border:solid 1px black; width: 200px;' id=" + "game" + i +"><button onclick='join()'>Присоединиться</button></div><br/> "); //выводим контейнеры для игр в столбец			
+					
+					for ( var x = 0; x < result.length; x++ ) //перебираем значения персонажа
+					{
+						$("#game"+ i ).append("<div style='display:inline-block;'>" + result[x] + "</div>");//выводим значения игр в строку
+					}
+				}
+			}
+		}
+	})
 }
 
 function join()
